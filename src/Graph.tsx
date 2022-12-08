5cc63096ca20ecd7dedc4fd912915eef869fd723
Bashir Dauda <56880803+bashiribndawud@users.noreply.github.com>
2022-12-08 15:25:09 +0100

    task completed

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -27,6 +27,9 @@ class Graph extends Component<IProps, {}> {
       top_ask_price: 'float',
       top_bid_price: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float'
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -44,6 +47,9 @@ class Graph extends Component<IProps, {}> {
         top_ask_price: 'avg',
         top_bid_price: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg'
       }));
     }
   }
@@ -51,7 +57,7 @@ class Graph extends Component<IProps, {}> {
   componentDidUpdate() {
     if (this.table) {
       this.table.update(
-        DataManipulator.generateRow(this.props.data),
+        [DataManipulator.generateRow(this.props.data), as unknown as TableData]
       );
     }
   }
